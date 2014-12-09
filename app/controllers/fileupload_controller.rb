4e654fb6444c7142b5e912c1b853c96557922e69
pramod <sharmapramod91@gmail.com>
2014-12-09 18:38:41 +0530

    User SignIn Or SignUp From task :Pramod

--- app/controllers/fileupload_controller.rb
+++
@@ -1,9 +0,0 @@
-class FileuploadController < ApplicationController
-  def upload 
-     render :file => 'app\views\fileupload\upload.html.erb'   
-  end
-   def uploadFile
-    post = DataFile.save(params[:upload])
-    render :text => "File has been uploaded successfully"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :companies do
     collection {post:import}
   end
-
   get 'welcome/index'
-  
-  resources :articles 
-  root 'welcome#index'
-  
+  resources :articles  
   get 'articles/new'
   get 'fileupload/upload'
+  
+root to: 'welcome#index'
 end
