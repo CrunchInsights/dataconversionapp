61e5fd1cd60a087c99a57e348d65c75589a5e729
sharmapramod <sharmapramod91@gmail.com>
2015-02-05 21:25:20 +0530

    aws file change|pramod

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,9 +1,6 @@
 
-#'AKIAJCN6M7H4D7O72GBA',
-#'3+4IJRYnoSBaqU3Tfvx3DceGtM1HFGfiH87IF9sK'
-#"crunchinsights-assets"
 AWS.config(
-            :access_key_id =>'AKIAJCN6M7H4D7O72GBA',#ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key => '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'#ENV['AWS_SECRET_ACCESS_KEY']             
+            :access_key_id =>ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']             
           )
-S3_BUCKET = AWS::S3.new.buckets["crunchinsights-assets"]#ENV['S3_BUCKET_NAME']
+S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET_NAME']]#
